6783b102bc92b092b4639435a4594bd3999247d3
DavitBakhutashvili <davit.bakhutashvili1@gmail.com>
2023-06-18 20:18:19 +0400

    feat: create admin component

---
+++ markup-app/src/Admin.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Admin = () => {
+  return (
+    <AdminPageWrapper>
+      {/* <Header /> */}
+      <AdminPageContainer>
+        {/* <Menu />
+        <Content /> */}
+      </AdminPageContainer>
+    </AdminPageWrapper>
+  );
+};
+
+export default Admin;
+
+const AdminPageWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #f8f8f8;
+  overflow: hidden;
+`;
+
+const AdminPageContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+`;
