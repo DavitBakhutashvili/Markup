8dc5ce2e5bfb14852ed6e99626f983a291f4d67e
DavitBakhutashvili <davit.bakhutashvili1@gmail.com>
2023-06-19 11:23:20 +0400

    fix: updated services

--- markup-app/src/helpers/myClass.js
+++ markup-app/src/helpers/myClass.js
@@ -1,4 +1,9 @@
 import axios from 'axios';
+import {
+  login,
+  refreshTokens,
+  sendTextToServer,
+} from '../services/useApiServices';
 
 class MyClass {
   constructor() {
@@ -11,7 +16,7 @@ class MyClass {
 
   start = async () => {
     // Login and get the initial tokens
-    await this.login();
+    await login.call(this);
 
     while (this.currentPos < this.text.length) {
       const endPos = this.calculateEndPos();
@@ -19,40 +24,13 @@ class MyClass {
       this.currentPos = endPos;
 
       // send sentence to the server
-      const result = await this.sendTextToServer(sentence);
+      const result = await sendTextToServer.call(this, sentence, this.token);
 
       // check if the onresult function exists and call it
       if (typeof this.onresult === 'function') {
         this.onresult(result);
       }
     }
-  };
-
-  login = async () => {
-    const response = await axios.post(
-      'https://enagramm.com/API/Account/Login',
-      {
-        Email: 'levan.lashauri1@gmail.com',
-        Password: 'Demo_1234',
-      }
-    );
-
-    this.token = response.data.AccessToken;
-
-    this.refreshToken = response.data.RefreshToken;
-  };
-
-  refreshTokens = async () => {
-    const response = await axios.post(
-      'https://enagramm.com/API/Account/RefreshToken',
-      {
-        AccessToken: this.token,
-        RefreshToken: this.refreshToken,
-      }
-    );
-
-    this.token = response.data.AccessToken;
-    this.refreshToken = response.data.RefreshToken;
   };
 
   calculateEndPos = () => {
@@ -63,37 +41,6 @@ class MyClass {
       }
     }
     return endPosLimit;
-  };
-
-  sendTextToServer = async (sentence) => {
-    const model = {
-      Language: 'ka',
-      Text: sentence,
-      Voice: 0,
-      iterationCount: 2,
-    };
-
-    try {
-      const response = await axios({
-        method: 'post',
-        url: 'https://enagramm.com/API/TTS/SynthesizeTextAudioPath',
-        data: JSON.stringify(model),
-        headers: {
-          Authorization: `Bearer ${this.token}`,
-          'Content-Type': 'application/json; charset=utf-8',
-        },
-      });
-      console.log('responsedata', response.data.AudioFilePath);
-      return response.data;
-    } catch (error) {
-      if (error.response && error.response.status === 401) {
-        // Refresh tokens and retry
-        await this.refreshTokens();
-        return this.sendTextToServer(sentence);
-      } else {
-        throw error;
-      }
-    }
   };
 }
 
